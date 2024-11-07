fe4f9792bc21109ad046d701c5bfa8b614a14524
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-11-07 21:25:01 +0700

    new

---
+++ pkg/routes/routeBook.go
@@ -0,0 +1,14 @@
+package routes
+
+import {
+	"github.com/gorilla/mux"
+	"github.com/joko345/goBook/pkg/control"
+}
+
+var RegisterBookRoutes = func(router *mux.Router){
+	router.HandleFunc("/book/", control.createBook).Methods("POST")
+	router.HandleFunc("/book/", control.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}",control.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}",control.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",control.DeleteBook).Methods("DELETE")
+}
