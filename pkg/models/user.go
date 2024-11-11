bd9ac81f1ab7dc96cdca44e043ea41af03d67e9c
JokoPrasetyoUtomo <joko345@users.noreply.github.com>
2024-11-11 16:36:24 +0700

    added user

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -7,21 +7,18 @@ import (
 
 var dbLogin *gorm.DB
 
-// Model User untuk menangani data pengguna
 type User struct {
 	ID       uint   `json:"id"`
 	Username string `json:"username"`
-	Password string `json:"password"` // Menyimpan password yang sudah di-hash untuk keamanan
+	Password string `json:"password"`
 }
 
-// Fungsi untuk membuat user baru
 func (u *User) CreateUser() *User {
-	dbLogin.NewRecord(u) // Membuat record baru
-	dbLogin.Create(&u)   // Menyimpan user ke database
+	dbLogin.NewRecord(u)
+	dbLogin.Create(&u)
 	return u
 }
 
-// Fungsi untuk mendapatkan user berdasarkan username
 func GetUserByUsername(username string) (User, error) {
 	var user User
 	if err := dbLogin.Where("username = ?", username).First(&user).Error; err != nil {
@@ -30,10 +27,9 @@ func GetUserByUsername(username string) (User, error) {
 	return user, nil
 }
 
-// Inisialisasi dan koneksi ke database
 func init() {
-	config.ConnectLogin()       // Menghubungkan ke database
-	dbLogin = config.GetLogin() // Mendapatkan instance DB dari config
-	// Auto-migrate untuk model User (hanya sekali saja pada aplikasi pertama)
+	config.ConnectLogin()
+	dbLogin = config.GetLogin()
+
 	dbLogin.AutoMigrate(&User{})
 }
